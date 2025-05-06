97c1ee2e93be4d0ef502f9933a35ce2bc1567644
Tx0sh1 <clydemojura@protonmail.com>
2025-05-06 20:30:07 +0200

    build basic prompt using cli_prompts crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use cli_prompts::{prompts::{Confirmation, Input, Multiselect}, DisplayPrompt};
+
 fn main() {
-    println!("Hello, world!");
+
+    loop {
+        let input_prompt = Input::new("Omni_shell >", |s| Ok(s.to_string())).help_message("enter command");
+
+        let command = input_prompt.display().unwrap();
+
+        if command == "exit"{
+            break;
+        } else {
+            println!("Omni_shell > {}", command)
+        }
+        
+    }
+    
 }
