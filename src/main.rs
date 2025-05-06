aae0b692620a02f40b9e82e6dd629b11a852f4a2
Tx0sh1 <clydemojura@protonmail.com>
2025-05-06 21:05:42 +0200

    add clear command

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use chrono::{DateTime, Utc};
 
 fn main() {
 
-    let now: DateTime<Utc> = Utc::now();
 
     loop {
+        let now: DateTime<Utc> = Utc::now();
         let input_prompt = Input::new("Omni_shell >", |s| Ok(s.to_string())).help_message("enter command");
 
         let command = input_prompt.display().unwrap();
@@ -13,6 +13,7 @@ fn main() {
 
         match command {
             "exit" => break,
+            "clear" => println!("\x1B[2J\x1B[H"),
             "date" => println!("UTC now is: {}", now),
             "help" => println!("Available commands: \n - help: Show this message \n - clear: Clear the screen \n - date: Show current date and time \n - exit: Exit the shell"),
             _ => println!("{}", command)
