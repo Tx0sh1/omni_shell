ffc92487d25e29b81901aca6fe18ac2e05ca61d2
Tx0sh1 <clydemojura@protonmail.com>
2025-05-06 21:00:26 +0200

    basic command logic using match

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use cli_prompts::{prompts::{Confirmation, Input, Multiselect}, DisplayPrompt};
+use chrono::{DateTime, Utc};
 
 fn main() {
+
+    let now: DateTime<Utc> = Utc::now();
 
     loop {
         let input_prompt = Input::new("Omni_shell >", |s| Ok(s.to_string())).help_message("enter command");
 
         let command = input_prompt.display().unwrap();
+        let command = command.trim();
 
-        if command == "exit"{
-            break;
-        } else {
-            println!("Omni_shell > {}", command)
+        match command {
+            "exit" => break,
+            "date" => println!("UTC now is: {}", now),
+            "help" => println!("Available commands: \n - help: Show this message \n - clear: Clear the screen \n - date: Show current date and time \n - exit: Exit the shell"),
+            _ => println!("{}", command)
         }
         
     }
